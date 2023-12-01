fe9868dfec8b3b873e08e1f1225e7e4e65ac5812
karthissit <karthissit@gmail.com>
2023-12-01 11:32:16 -0500

    Updated loops

--- loops/loops.go
+++ loops/loops.go
@@ -1,5 +1,36 @@
 package loops
 
+import "fmt"
+
 func LoopsExample() {
 
+	//only one loop in go lang "for" ==> can be used as while, do while, for, for each
+
+	// regular
+	count := 10
+	for i := 0; i < count; i++ {
+		fmt.Printf("In loop %d\n", i)
+	}
+
+	//while
+	for 0 < count {
+		fmt.Printf("Decremented to: %d", count)
+		count--
+	}
+
+	//do while
+	b := 0
+	for {
+		if b > 2 {
+			break
+		}
+		fmt.Println(b)
+		b++
+	}
+
+	//for each
+	intslice := []int{1, 2, 3, 4, 5}
+	for i, num := range intslice {
+		fmt.Printf("Index: %d, Number: %d", i, num)
+	}
 }
